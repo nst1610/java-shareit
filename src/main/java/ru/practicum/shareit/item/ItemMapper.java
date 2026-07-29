5a68c6e1cf95491101aff44cb8770b4023c7ef4e
anastasia.semenova <anastasia.semenova@mediascope.net>
2026-07-29 23:30:17 +0300

    Добавлена логика работы с вещами

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item;
 
-import lombok.experimental.UtilityClass;
 import org.mapstruct.Mapper;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
